5c2a4f867555540463f0c00631044d936d4c7c27
mahuanfy <mahuanfy@foxmail.com>
2020-07-27 22:04:28 +0800

    [Huanhuan]: refactor the code.

--- app/src/main/java/com/example/roonrxjava/db/PersonDatabase.java
+++ app/src/main/java/com/example/roonrxjava/db/PersonDatabase.java
@@ -9,17 +9,16 @@ import androidx.room.RoomDatabase;
 import com.example.roonrxjava.entity.Person;
 
 @Database(entities = {Person.class}, version = 1, exportSchema = false)
-public abstract class PersonDatabase extends RoomDatabase   {
+public abstract class PersonDatabase extends RoomDatabase {
     private static volatile PersonDatabase INSTANCE;
+
     public abstract PersonDao personDao();
 
     public static PersonDatabase getPersonInstance(Context context) {
         if (INSTANCE == null) {
             synchronized (PersonDatabase.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), PersonDatabase.class, "Person")
-                            .build();
-                }
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(), PersonDatabase.class, "Person")
+                        .build();
             }
         }
         return INSTANCE;
